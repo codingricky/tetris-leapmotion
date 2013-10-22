06e6bf76c3c9b0fc464ad003462bf20c9d089d38
Ricky <ricky@dius.com.au>
2013-10-22 17:21:52 +1100

    added images

--- tetris-game/src/main/java/tetris/game/Game.java
+++ tetris-game/src/main/java/tetris/game/Game.java
@@ -1,10 +1,16 @@
 package tetris.game;
 
+import core.tetris.PieceType;
+import core.tetris.TetrisGame;
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * A game using Slick2d
@@ -15,27 +21,45 @@ public class Game extends BasicGame {
     private static final int WIDTH = 800;
     /** Screen height */
     private static final int HEIGHT = 600;
-    
-    /** A counter... */
-    private int counter;
+
+    private TetrisGame tetrisGame;
+    private Map<PieceType, Image> pieceTypeToImageMap = new HashMap<>();
 
     public Game() {
         super("A Slick2d game");
     }
 
     public void render(GameContainer container, Graphics g) throws SlickException {
-        g.drawString("Hello, " + Integer.toString(counter) + "!", 50, 50);
-
+        for (int x = 0; x < tetrisGame.getX(); x++) {
+            for (int y = 0; y < tetrisGame.getY(); y++) {
+                PieceType pieceAt = tetrisGame.getPieceAt(x, y);
+                if (pieceAt != null) {
+                    Image image = pieceTypeToImageMap.get(pieceAt);
+                    int xPosition = x * image.getWidth();
+                    int yPosition = y * image.getHeight();
+                    g.drawImage(image, xPosition, yPosition);
+                }
+            }
+        }
     }
 
     @Override
     public void init(GameContainer container) throws SlickException {
-        counter = 0;
+        tetrisGame = new TetrisGame();
+        tetrisGame.startGame();
+
+        pieceTypeToImageMap.put(PieceType.I_PIECE, new Image("images/aqua.png"));
+        pieceTypeToImageMap.put(PieceType.J_PIECE, new Image("images/blue.png"));
+        pieceTypeToImageMap.put(PieceType.O_PIECE, new Image("images/green.png"));
+        pieceTypeToImageMap.put(PieceType.L_PIECE, new Image("images/orange.png"));
+        pieceTypeToImageMap.put(PieceType.S_PIECE, new Image("images/purple.png"));
+        pieceTypeToImageMap.put(PieceType.T_PIECE, new Image("images/red.png"));
+        pieceTypeToImageMap.put(PieceType.Z_PIECE, new Image("images/yellow.png"));
     }
 
     @Override
     public void update(GameContainer container, int delta) throws SlickException {
-        counter++;
+        tetrisGame.tick();
     }
     
     public static void main(String[] args) throws SlickException {
